77b8bb7b9b3a15c0c7cb3d2a363d8092d2bacfc9
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 08:00:28 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Meeting Scheduler</h1>
+      <p>Welcome to the meeting scheduler application!</p>
+    </div>
+  );
+}
+
+export default App;
